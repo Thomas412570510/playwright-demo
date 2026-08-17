cc7ab01ba4f456a1cb56bf169cbe8f29f3c5ec3d
Aydan3199 <144983382+Aydan3199@users.noreply.github.com>
2026-08-17 13:35:16 +0800

    Configure perfect debug strategy: codegen trace script + emergency screenshots on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
     baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
     video: 'on',
   },
 
